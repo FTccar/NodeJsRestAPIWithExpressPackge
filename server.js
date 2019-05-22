b10f9d8e9f9f186b1bbf9d605f74b954c640e600
Ferdi Tüccar <ferdi.tuccar@outlook.com>
2019-05-22 13:24:21 +0300

    İntial Commit

---
+++ server.js
@@ -0,0 +1,33 @@
+var http = require('http')
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var cities = [{name: 'Istanbul', country: 'Turkey'}, {name: 'New York', country: 'USA'}, {name: 'London', country:'England'}];
+
+var app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
+
+	
+app.get('/api/cities', function(request, response){
+   response.send(cities);
+});
+
+app.post('/api/cities',function(request,response){
+    var city = request.body;
+    for(var index =0; index <cities.length; index++)
+    {
+        if(cities[index].name == city.name){
+            response.status(500).send({error:"Bu şehir zaten var!!"});
+            return;
+        }
+    }
+    cities.push(city);
+    response.send(cities);
+})
+
+app.listen(1923,function(){
+    console.log("Port dinleniyor: 1923..");
+})
